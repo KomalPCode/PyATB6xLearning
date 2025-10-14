4691680e7059f36f714e90ba4e3fbe0a91ce0458
KomalPCode <komalgithub1507@gmail.com>
2025-10-14 14:34:44 +0530

    14th Oct Programs for Task

---
+++ Task/Task_14 Oct_001.py
@@ -0,0 +1,5 @@
+#Write a Python program to calculate the area of triangle using the formula
+base = float(input("Enter the base of triangle : "))
+height = float(input("Enter the height of triangle : "))
+area = 0.5 * base * height
+print(f"The area of the triangle is {area:.2f}")
---
+++ Task/Task_14 Oct_002.py
@@ -0,0 +1,5 @@
+#Write a program to check if number entered by user is greater than 100
+
+num = int(input("Enter any number : "))
+print("Number is greater than 100" if num > 100 else "Number is less than 100" )
+print("Entered number is : ", num)
