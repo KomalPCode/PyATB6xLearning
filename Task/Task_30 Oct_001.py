6a58da11e8808834c0ec1e9120c73c716d2d5fa5
KomalPCode <komalgithub1507@gmail.com>
2025-10-31 12:08:52 +0530

    30th Oct Tasks changes

--- Task/Task_30 Oct_001.py
+++ Task/Task_30 Oct_001.py
@@ -1,3 +1,11 @@
+"""
+Q - Create a function which will take a positive number from the user and perform square of the number?
+
+i/o = 3
+
+o/p = 9
+
+"""
 def find_square(number):
     if number > 0:
         square = number * number
--- Task/Task_30 Oct_002.py
+++ Task/Task_30 Oct_002.py
@@ -1,10 +1,16 @@
+"""
+Q - Create a function which will take the 3 values from the user, which are length of the triangle.  side1, side2, side2
+i/p - int side1 == side2 =side3 → isoceles
+o/p = result in string - iso, eq, scalene
+
+"""
 def area_Triangle(side1, side2, side3):
     if side1 > 0 and side2 > 0 and side3 > 0:
         if side1 == side2 == side3:
             return "It's a Equilateral triangle"
         elif side1 == side2 or side1 == side3 or side2 == side3:
             return "It's a Isosceles triangle"
-        elif side1 != side2 != side3:
+        else:
             return "It's a Scalene triangle"
 
 
